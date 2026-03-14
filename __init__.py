b5ff773951fc392d630f94ce238ae7bdb6098490
Martijn van Duijneveldt <martijnvanduijneveldt@gmail.com>
2026-03-14 16:16:09 +0100

    Allow nevermore with custom names

--- __init__.py
+++ __init__.py
@@ -5,7 +5,12 @@ from .esphome_nevermore_library.nevermore import Nevermore
 
 __all__ = [
     "load_config",
+    "load_config_prefix"
 ]
 
 def load_config(config: ConfigWrapper):
     return Nevermore(config)
+
+# Allow custom names
+def load_config_prefix(config: ConfigWrapper):
+    return load_config(config)
